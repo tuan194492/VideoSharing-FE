910c8591f18da12765aecf3c8ff25470a5981dc9
tuan194492 <tuan1216302001@gmail.com>
2024-04-29 17:02:04 +0700

    Init socket for real time communication

--- src/context/Socket.js
+++ src/context/Socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 const baseServerUrl = `${process.env.BASE_SERVER}`;
 
-const socket = io(`${baseServerUrl}`);
+const socket = io("http://localhost:2999");
 
+socket.connect();
 export default socket;
